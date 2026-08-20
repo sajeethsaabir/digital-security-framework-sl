ca1274253334d5673dc5fa0b9e2d0c8e975afaca
sajeethsaabir <sajiiahd@gmail.com>
2026-08-20 18:22:34 +0530

    feat: add gitignore file

--- backend/dist/search/search.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from 'express';
-import { DbService } from '../db/db.service';
-export declare class SearchController {
-    private db;
-    constructor(db: DbService);
-    search(q: string, res: Response): Promise<Response<any, Record<string, any>>>;
-}
